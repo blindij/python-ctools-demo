122985bebec13fdbb2da247da7af5e55eec42822
Bjorn Lindi <blindij@users.noreply.github.com>
2017-06-09 09:27:06 +0200

    Added source files

---
+++ cmake-demo/src/taylor_series.h
@@ -0,0 +1,9 @@
+// file: taylor_series.h
+#ifndef TAYLOR_SERIES_H_
+#define TAYLOR_SERIES_H_
+
+unsigned long long factorial( int n);
+double ts_sin(double& x, int N);
+double ts_cos(double& x, int N);
+
+#endif // TAYLOR_SERIES_H_
