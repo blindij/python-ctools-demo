6b13e6cab1a5694813d86aeeb8cd4e24d585d55c
Bjorn Lindi <blindij@users.noreply.github.com>
2017-06-16 11:49:38 +0200

    Corrected comment about pybind11 subdirectory - that it is a add submodule. Changed from PYBIND_PLUGIN to PYBIND_MODULE in the binding file

--- pybind11-demo/src/py11taylor.cpp
+++ pybind11-demo/src/py11taylor.cpp
@@ -4,9 +4,8 @@
 
 namespace py = pybind11;
 
-PYBIND11_PLUGIN(taylor) {
-  py::module t("taylor", "Taylor Series tailored plugin");
+PYBIND11_MODULE(taylor,t) {
+  t.doc() = "Taylor Series tailored plugin";
   t.def("sin", &ts_sin, "A sine Taylor Series function")
    .def("cos", &ts_cos, "A cosine Taylor Series function");
-  return t.ptr();
 }
